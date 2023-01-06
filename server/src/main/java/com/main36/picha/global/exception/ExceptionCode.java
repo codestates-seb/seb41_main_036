41c5a28bce66eb225f99dcf1c46a1443b47545d7
Sangyoo <tkddbwkd@naver.com>
2023-01-06 18:22:25 +0900

    chore global 파일 추가

---
+++ server/src/main/java/com/main36/picha/global/exception/ExceptionCode.java
@@ -0,0 +1,31 @@
+package com.main36.picha.global.exception;
+
+import lombok.Getter;
+
+public enum ExceptionCode {
+    MEMBER_NOT_FOUND(404, "Member not found"),
+    MEMBER_EXISTS(409, "Member exists"),
+
+    LOCATION_NOT_FOUND(404, "Location not found"),
+    LOCATION_EXISTS(409, "Location exists"),
+
+    COMMENT_NOT_FOUND(404, "Comment not found"),
+    COMMENT_EXISTS(409, "Comment exists"),
+
+    HASHTAG_NOT_FOUND(404, "HashTag not found"),
+    HASHTAG_EXISTS(409, "HashTag exists"),
+
+    POST_NOT_FOUND(404, "Post not found"),
+    POST_EXISTS(409, "Post exists");
+
+    @Getter
+    private int status;
+
+    @Getter
+    private String message;
+
+    ExceptionCode(int code, String message) {
+        this.status = code;
+        this.message = message;
+    }
+}
