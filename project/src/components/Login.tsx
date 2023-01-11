8236c2d3dcc3fd4a13caf3ef2d991084f76e2bf4
Lee SeungHyun <tmdgusdlrk2@gmail.com>
2023-01-12 04:20:43 +0900

    로그인&회원가입 페이지 폼 완성

--- project/src/components/Login.tsx
+++ project/src/components/Login.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
+// import {useState, useEffect} from "react"
 
 interface TextProps {
     fontSize: string;
@@ -8,6 +9,9 @@ interface TextProps {
 }
 interface PaddingProps {
     padding: string;
+}
+interface OverlayProps {
+    overlay: boolean;
 }
 
 const Wraoper = styled.section`
@@ -18,9 +22,9 @@ const Wraoper = styled.section`
     align-items: center;
     border-radius: 30px;
     border: 1px solid black;
-
-    > .logincontainer{
-    width: 50%;
+`
+const Logincontainer  =styled.div<OverlayProps>`
+    width: 40%;
     height: 100%;
     border: 1px solid red;
     border-radius: 30px 0px 0px 30px;
@@ -30,9 +34,12 @@ const Wraoper = styled.section`
     align-items: center;
     background-color: white;
     transition: all 0.5s;
-    }
-    > .signincontainer{
-        width: 50%;
+    position: absolute;
+    z-index: ${(props) => props.overlay ? '1': '0'};
+    transform: ${(props) => props.overlay ? 'translateX(-50%)': 'translateX(50%)'};
+    `
+const Signincontainer = styled.div<OverlayProps>`
+    width: 40%;
     height: 100%;
     border: 1px solid red;
     border-radius: 0px 30px 30px 0px;
@@ -42,10 +49,12 @@ const Wraoper = styled.section`
     align-items: center;
     background-color: white;
     transition: all 0.5s;
-    
-    }
-    > .leftoverlay{
-        width: 50%;
+    position: absolute;
+    z-index: ${(props) => props.overlay ? '0': '1'};
+    transform: ${(props) => props.overlay ? 'translateX(-50%)': 'translateX(50%)'};
+    `
+const Leftoverlay = styled.div<OverlayProps>`
+    width: 40%;
     height: 100%;
     border: 1px solid red;
     opacity: 1;
@@ -58,10 +67,12 @@ const Wraoper = styled.section`
     transition: all 0.5s;
     justify-content: center;
     color: white;
-
-    }
-    > .rightoverlay{
-        width: 50%;
+    position: absolute;
+    z-index: ${(props) => props.overlay ? '0': '1'};
+    transform: ${(props) => props.overlay ? 'translateX(50%)': 'translateX(-50%)'};
+`
+const Rightoverlay = styled.div<OverlayProps>`
+        width: 40%;
     height: 100%;
     border: 1px solid red;
     border-radius: 0px 30px 30px 0px;
@@ -73,7 +84,9 @@ const Wraoper = styled.section`
     transition: all 0.5s;
     justify-content: center;
     color: white;
-    }
+    position: absolute;
+    z-index: ${(props) => props.overlay ? '1': '0'};
+    transform: ${(props) => props.overlay ? 'translateX(50%)': 'translateX(-50%)'};
 `
 const LoginButton = styled.button`
     width: 180px;
@@ -100,7 +113,6 @@ const LoginButton2 = styled.button`
         background-color: var(--purple-400);
     }
 `
-
 const GButton = styled.button`
     width: 50px;
     height: 50px;
@@ -114,17 +126,15 @@ const GButton = styled.button`
         background-color: var(--purple-400);
     }
 `
-
-
-const InputPadding = styled.label`
-    width: 30px;
-    height: 45px;
-    padding-bottom: 15px;
-    padding-left: 15px;
-    border-bottom: 1px solid var(--black-400);
-    color: var(--black-500);
+// const InputPadding = styled.label`
+//     width: 30px;
+//     height: 45px;
+//     padding-bottom: 15px;
+//     padding-left: 15px;
+//     border-bottom: 1px solid var(--black-400);
+//     color: var(--black-500);
     
-`
+// `
 const InputStyle = styled.input`
     width: 62%;
     height: 45px;   
@@ -143,7 +153,6 @@ const TextStyle = styled.div<TextProps>`
     font-weight: ${(props)=>props.fontweight};
     padding-top: 10px;
 `
-
 const CustomPadding = styled.div<PaddingProps>`
     padding: ${(props)=>props.padding};
 `
@@ -155,73 +164,158 @@ const LogoLogo = styled.div`
     color: black;
     font-size: 50px;
 `
-// transform: translateX(100%);
+const FormForm = styled.form`
+    display: flex;
+    flex-direction: column;
+    height: 100%;
+    justify-content: center;
+    align-items: center;
+`
 
 const Login  =  () => {
+    const [overlays, setOverlays] = useState(false)
+    const [loginemail, setLoginEmail] = useState('')
+    const [loginemailErr, setLoginEmailErr] = useState(false);
+    const [signemail, setSignEmail] = useState('')
+    const [signemailErr, setSignEmailErr] = useState(false);
+    const [signpassword, setSignPassword] = useState('')
+    const [signpasswordErr, setSignPasswordErr] = useState(false);
+    const [loginpassword, setLoginPassword] = useState('')
+    const [loginpasswordErr, setLoginPasswordErr] = useState(false);
+    const [passwordConfirm, setPasswordConfirm] = useState('');
+
+    const [phonenumber, setPhonenumber] = useState('')
+    const [phonenumberErr, setPhonenumberErr] = useState(false)
+
+    // const [nickname, setNickname] = useState('')
+    // const [address, setAddress] = useState('')
+
+    const onClickBtn = (e:React.MouseEvent<HTMLButtonElement>) =>{
+        setOverlays(!overlays)
+    }
+    const handleLoginEmailChange = (e:React.ChangeEvent<HTMLInputElement>) =>{
+    const EMAIL_REGEX = /\w+([-+.]\w+)*@\w+([-.]\w+)*\.[a-zA-Z]{2,4}$/;
+    if (EMAIL_REGEX.test(e.target.value)) {
+        setLoginEmailErr(false);
+    } else {
+        setLoginEmailErr(true);
+    }
+    setLoginEmail(e.target.value);
+    };
+
+    const handleSignEmailChange = (e:React.ChangeEvent<HTMLInputElement>) =>{
+        const EMAIL_REGEX = /\w+([-+.]\w+)*@\w+([-.]\w+)*\.[a-zA-Z]{2,4}$/;
+        if (EMAIL_REGEX.test(e.target.value)) {
+            setSignEmailErr(false);
+        } else {
+            setSignEmailErr(true);
+        }
+        setSignEmail(e.target.value);
+        };
+
+    const handleLoginPasswordChange = (e:React.ChangeEvent<HTMLInputElement>) =>{
+        if (e.target.value.length >= 8) {
+            setLoginPasswordErr(false);
+        } else {
+            setLoginPasswordErr(true);
+        }
+        setLoginPassword(e.target.value);
+        };
+
+    const handleSignPasswordChange = (e:React.ChangeEvent<HTMLInputElement>) =>{
+        if (e.target.value.length >= 8) {
+            setSignPasswordErr(false);
+            } else {
+            setSignPasswordErr(true);
+        }
+        setSignPassword(e.target.value);
+        };
+
+    const handlePasswordConfirm = (e:React.ChangeEvent<HTMLInputElement>) => {
+        setPasswordConfirm(e.target.value)
+    }
+
+    const handlePhoneChange = (e:React.ChangeEvent<HTMLInputElement>) =>{
+        const PHONE_REGEX = /01[016789]-[^0][0-9]{2,3}-[0-9]{4}/;
+            if (PHONE_REGEX.test(e.target.value)) {
+                setPhonenumberErr(false);
+            } else {
+                setPhonenumberErr(true);
+            }
+            setPhonenumber(e.target.value);
+            };
+
+
     return (
-     <Wraoper>
+        <Wraoper>
             {/* <div>
             <InputPadding>이메일</InputPadding>
             <InputStyle placeholder='이메일'></InputStyle>
-            </div> */}
-        <div className='logincontainer'>
+        </div> */}
+        <Logincontainer overlay={overlays}>
             <TextStyle color="#6154F8" fontSize='45px' fontweight='bold'>로그인</TextStyle>
             <CustomPadding padding='30px 0px 0px 0px'></CustomPadding>
             <GButton>G</GButton>
             <TextStyle color="#6154F8" fontSize='22px' fontweight='bold'>SNS 계정으로 로그인</TextStyle>
             <CustomPadding padding='30px 0px 0px 0px'></CustomPadding>
-            <InputStyle placeholder='이메일'></InputStyle>
-            <TextStyle color="black" fontSize='18px' fontweight='normal'>올바른 이메일 형식이 아닙니다.</TextStyle>
-            <InputStyle placeholder='비밀번호'></InputStyle>
-            <TextStyle color="black" fontSize='18px' fontweight='normal'>비밀번호를 8자이상 입력해주세요.</TextStyle>
+            <InputStyle placeholder='이메일' onChange={handleLoginEmailChange}></InputStyle>
+            {loginemailErr ? (
+                <TextStyle color="black" fontSize='18px' fontweight='normal'>올바른 이메일 형식이 아닙니다.</TextStyle>
+                ): <></>}
+            <InputStyle placeholder='비밀번호' onChange={handleLoginPasswordChange}></InputStyle>
+            {loginpasswordErr ?(
+                <TextStyle color="black" fontSize='18px' fontweight='normal'>비밀번호를 8자이상 입력해주세요.</TextStyle>
+            ): <></>}
             <CustomPadding padding='50px 0px 0px 0px'></CustomPadding>
             <LoginButton2>로그인</LoginButton2>
             <CustomPadding padding='20px 0px 0px 0px'></CustomPadding>
             <TextStyle color="#6154F8" fontSize='22px' fontweight='bold' >계정정보를 잊으셨나요?</TextStyle>
-        </div>
-        <div className='signincontainer'>
+        </Logincontainer>
+        <Signincontainer overlay={overlays}>
             <TextStyle color="#6154F8" fontSize='45px' fontweight='bold'>회원가입</TextStyle>
             <CustomPadding padding='30px 0px 0px 0px'></CustomPadding>
             <GButton>G</GButton>
             <TextStyle color="#6154F8" fontSize='22px' fontweight='bold'>SNS 계정으로 가입하기</TextStyle>
-            <InputStyle placeholder='이메일'></InputStyle>
-            <TextStyle color="black" fontSize='18px' fontweight='normal'>올바른 이메일 형식이 아닙니다.</TextStyle>
-            <InputStyle placeholder='비밀번호'></InputStyle>
-            <TextStyle color="black" fontSize='18px' fontweight='normal'>비밀번호를 8자이상 입력해주세요.</TextStyle>
-            <InputStyle placeholder='비밀번호확인'></InputStyle>
-            <TextStyle color="black" fontSize='18px' fontweight='normal'>비밀번호가 다릅니다.</TextStyle>
-            <InputStyle placeholder='전화번호'></InputStyle>
-            <TextStyle color="black" fontSize='18px' fontweight='normal'>올바른 전화번호 형식이 아닙니다.<div className=""></div></TextStyle>
+            <InputStyle placeholder='이메일' onChange={handleSignEmailChange}></InputStyle>
+            {signemailErr ? (
+                <TextStyle color="black" fontSize='18px' fontweight='normal'>올바른 이메일 형식이 아닙니다.</TextStyle>
+                ): <></>}
+            <InputStyle placeholder='비밀번호' onChange={handleSignPasswordChange}></InputStyle>
+            {signpasswordErr ?(
+                <TextStyle color="black" fontSize='18px' fontweight='normal'>비밀번호를 8자이상 입력해주세요.</TextStyle>
+            ): <></>}
+            <InputStyle placeholder='비밀번호확인' onChange={handlePasswordConfirm}></InputStyle>
+            {passwordConfirm === signpassword ? <></> :(
+                <TextStyle color="black" fontSize='18px' fontweight='normal'>비밀번호가 다릅니다.</TextStyle>
+            )}
+            <InputStyle placeholder='전화번호(-를 포함해서 입력해주세요)' onChange={handlePhoneChange}></InputStyle>
+            {phonenumberErr ? (
+                <TextStyle color="black" fontSize='18px' fontweight='normal'>올바른 전화번호 형식이 아닙니다.<div className=""></div></TextStyle>
+            ):<></>}
             <InputStyle placeholder='주소'></InputStyle>
-            <TextStyle color="black" fontSize='18px' fontweight='normal'>서울시 OO구 형식이 아닙니다.</TextStyle>
+            {/* <TextStyle color="black" fontSize='18px' fontweight='normal'>서울시 OO구 형식이 아닙니다.</TextStyle> */}
             <InputStyle placeholder='닉네임'></InputStyle>
-            <TextStyle color="black" fontSize='18px' fontweight='normal'>3글자 이상 입력해주세요.<div className=""></div></TextStyle>
+            {/* <TextStyle color="black" fontSize='18px' fontweight='normal'>3글자 이상 입력해주세요.<div className=""></div></TextStyle> */}
             <CustomPadding padding='50px 0px 0px 0px'></CustomPadding>
             <LoginButton2>회원가입</LoginButton2>
             <CustomPadding padding='20px 0px 0px 0px'></CustomPadding>
             <TextStyle color="#6154F8" fontSize='22px' fontweight='bold' >계정정보를 잊으셨나요?</TextStyle>
-        </div>
-
-        <div className='leftoverlay'>
+        </Signincontainer>
+        <Leftoverlay overlay={overlays}>
             <TextStyle color="white" fontSize='25px' fontweight='normal'>welcome to the</TextStyle>
             <LogoLogo>pickcha</LogoLogo>
             <CustomPadding padding='70px 0px 0px 0px'></CustomPadding>
-
-            <TextStyle color="white" fontSize='40px' fontweight='normal'>사진찍기 가장 좋은 장소는 어디일까요?</TextStyle>
+            <TextStyle color="white" fontSize='30px' fontweight='normal'>사진찍기 가장 좋은 장소는 어디일까요?</TextStyle>
             <CustomPadding padding='30px 0px 0px 0px'></CustomPadding>
-
             <TextStyle color="white" fontSize='18px' fontweight='normal'>pickcha에서 명소부터 포스트까지</TextStyle>
             <TextStyle color="white" fontSize='18px' fontweight='normal'>다양한 정보를 통해 나만의 사진을 찍어보세요.</TextStyle>
             <CustomPadding padding='70px 0px 0px 0px'></CustomPadding>
-
             <TextStyle color="white" fontSize='18px' fontweight='bold'>이미 회원이시라면?</TextStyle>
             <CustomPadding padding='20px 0px 0px 0px'></CustomPadding>  
-
-            <LoginButton>로그인</LoginButton>
-        </div>
-        <div className='rightoverlay'>
-        <div>welcome to the</div>
-        <TextStyle color="white" fontSize='25px' fontweight='normal'>welcome to the</TextStyle>
+            <LoginButton onClick={onClickBtn}>로그인</LoginButton>
+        </Leftoverlay>
+        <Rightoverlay overlay={overlays}>
+            <TextStyle color="white" fontSize='25px' fontweight='normal'>welcome to the</TextStyle>
             <LogoLogo>pickcha</LogoLogo>
             <CustomPadding padding='70px 0px 0px 0px'></CustomPadding>
             <TextStyle color="white" fontSize='30px' fontweight='normal'>사진찍기 가장 좋은 장소는 어디일까요?</TextStyle>
@@ -231,11 +325,10 @@ const Login  =  () => {
             <CustomPadding padding='70px 0px 0px 0px'></CustomPadding>
             <TextStyle color="white" fontSize='18px' fontweight='bold'>아직 회원이 아니신가요?</TextStyle>
             <CustomPadding padding='20px 0px 0px 0px'></CustomPadding>
-            <LoginButton>회원가입</LoginButton>
-        </div>
-     </Wraoper>
+            <LoginButton onClick={onClickBtn}>회원가입</LoginButton>
+        </Rightoverlay>
+    </Wraoper>
     );
-  }
-  
-  export default Login;
-  
+}
+
+export default Login;
