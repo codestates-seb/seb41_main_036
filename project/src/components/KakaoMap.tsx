340aacbf64b876d49899a77dccbce7869d53836a
부들이 <84694944+namnam-5799@users.noreply.github.com>
2023-01-13 02:51:57 +0900

    Feat kakao map (#26)
    * Rename .gitigore to .gitignore
    
    * fix: 명소 상세페이지 수정
    
    * fix: 명소 상세페이지 수정
    
    * feat: 카카오맵 컨트롤
    
    * fix: feat_front pull
    
    Co-authored-by: Sangyoo <tkddb7347@gmail.com>

--- project/src/components/KakaoMap.tsx
+++ project/src/components/KakaoMap.tsx
@@ -21,10 +21,16 @@ const KakaoMap = () =>{
   useEffect(()=>{
     const container = document.getElementById('map');// 지도를 담을 dom영역
     const options = {
+      // center에 위도, 경도 좌표를 설정 
       center: new window.kakao.maps.LatLng(37.573898277022,126.9731314753), // 지도의 중심 좌표
-      level:3
+      level:3 // 확대되어 보여지는 레벨  설정 
     };
     const map = new window.kakao.maps.Map(container,options);
+
+    // 확대, 축소 컨트롤을 생성 
+    var zoomControl = new window.kakao.maps.ZoomControl();
+    map.addControl(zoomControl, window.kakao.maps.ControlPosition.RIGHT);
+
 
     // marker로 원하는 위도, 경도의 값을 나타냅니다. 
     var marker = new window.kakao.maps.Marker({
@@ -32,22 +38,22 @@ const KakaoMap = () =>{
       position:options.center
     })
 
-    var infoWindow = new window.kakao.maps.InfoWindow({
-      content: info,
-      removable:true
-    })
-    infoWindow.open(map,marker)
+    // var infoWindow = new window.kakao.maps.InfoWindow({
+    //   content: info,
+    //   removable:true
+    // })
+    // infoWindow.open(map,marker)
 
-    window.kakao.maps.event.addListener(marker, "click", () => {
-      // 마커 위에 인포윈도우를 표시
-      infoWindow.open(map, marker);
-    });
+    // window.kakao.maps.event.addListener(marker, "click", () => {
+    //   // 마커 위에 인포윈도우를 표시
+    //   infoWindow.open(map, marker);
+    // });
 
   },[])
 
   return(
     <>
-    <div id="map" style={{width:"800px",height:"500px"}}></div>
+    <div id="map" style={{width:"840px",height:"500px"}}></div>
     </>
   )
 }
