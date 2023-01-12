e97153c35b80e55e9dfc7a2465564c107f2b5075
MINSE97 <99317339+MINSE97@users.noreply.github.com>
2023-01-13 00:47:36 +0900

    Feature: 명소 페이지 생성 (#20)
    * design: 페이지 레이아웃 생성
    
    * chore: branch 이름 변경
    
    * feat: 지역 선택 기능 생성
    
    * feat: 더미데이터 생성
    
    * feat: 더미데이터 연결 및 지역구 선택 기능 생성
    
    * fix: 더미데이터 수정
    
    * fix: 오타 수정

--- project/src/Main.tsx
+++ project/src/Main.tsx
@@ -1,36 +1,36 @@
-import styled from 'styled-components';
-import KakaoMap from './components/KakaoMap';
+import styled from "styled-components";
+// import KakaoMap from "./components/KakaoMap";
+import Place from "./pages/Place";
 
 
 const Header = styled.div`
-  width:100% ;
+  width: 100%;
   background-color: pink;
   height: 157px;
-`
+`;
 
 const Body = styled.div`
-  background-color: orange;
   width: 83.5%;
   margin: 0 auto;
   height: 100vh;
-`
+`;
 
 const Footer = styled.div`
-  width:100% ;
+  width: 100%;
   background-color: skyblue;
   height: 157px;
-`
+`;
 
-function Main () {
-  return(
+function Main() {
+  return (
     <>
-    <Header>header헤더</Header>
-    <Body>
-
-    </Body>
-    <Footer>footer</Footer>
+      <Header>header헤더</Header>
+      <Body>
+        <Place />
+      </Body>
+      <Footer>footer</Footer>
     </>
-  )
+  );
 }
 
 export default Main;
