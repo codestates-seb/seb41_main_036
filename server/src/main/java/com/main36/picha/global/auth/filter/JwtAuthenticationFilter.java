66c6687de137e485cb943e54288792158841dfac
eastmeet <ys932184@gmail.com>
2023-01-12 14:49:01 +0900

    feat: Add authentication success & failure handler(#16)

--- server/src/main/java/com/main36/picha/global/auth/filter/JwtAuthenticationFilter.java
+++ server/src/main/java/com/main36/picha/global/auth/filter/JwtAuthenticationFilter.java
@@ -25,54 +25,16 @@ import java.util.Map;
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
     private final AuthenticationManager authenticationManager;
-    private final JwtTokenizer jwtTokenizer;
-
 
     @SneakyThrows
     @Override
     public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) {
         ObjectMapper objectMapper = new ObjectMapper();
         LoginDto loginDto = objectMapper.readValue(request.getInputStream(), LoginDto.class);
-
         UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
                 loginDto.getUsername(), loginDto.getPassword());
 
         return authenticationManager.authenticate(authenticationToken);
     }
 
-    @Override
-    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException, ServletException {
-        Member member = (Member) authResult.getPrincipal();
-
-        String accessToken = delegateAccessToken(member);
-        String refreshTokne = delegateRefreshToken(member);
-
-        response.setHeader("Authorization", "Bearer " + accessToken);
-        response.setHeader("Refresh", refreshTokne);
-    }
-
-    private String delegateAccessToken(Member member) {
-        Map<String, Object> claims = new HashMap<>();
-        claims.put("username", member.getEmail());
-        claims.put("roles", member.getRoles());
-
-        String subject = member.getEmail();
-        Date expiration = jwtTokenizer.getTokenExpiration(jwtTokenizer.getAccessTokenExpirationMinutes());
-
-        String base64EncodedSecretKey = jwtTokenizer.encodeBase64SecretKey(jwtTokenizer.getSecretKey());
-
-        String accessToken = jwtTokenizer.generatedAccessToken(claims, subject, expiration, base64EncodedSecretKey);
-
-        return accessToken;
-    }
-
-    private String delegateRefreshToken(Member member) {
-        String subject = member.getEmail();
-        Date expiration = jwtTokenizer.getTokenExpiration(jwtTokenizer.getRefreshTokenExpirationMinutes());
-        String base64EncodedSecretKey = jwtTokenizer.encodeBase64SecretKey(jwtTokenizer.getSecretKey());
-
-        String refreshToken = jwtTokenizer.generateRefreshToken(subject, expiration, base64EncodedSecretKey);
-
-        return refreshToken;
-    }
 }
--- server/src/main/java/com/main36/picha/global/config/CustomFilterConfigure.java
+++ server/src/main/java/com/main36/picha/global/config/CustomFilterConfigure.java
@@ -1,6 +1,8 @@
 package com.main36.picha.global.config;
 
 import com.main36.picha.global.auth.filter.JwtAuthenticationFilter;
+import com.main36.picha.global.auth.handler.MemberAuthenticationFailureHandler;
+import com.main36.picha.global.auth.handler.MemberAuthenticationSuccessHandler;
 import com.main36.picha.global.auth.jwt.JwtTokenizer;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -16,9 +18,10 @@ public class CustomFilterConfigure extends AbstractHttpConfigurer<CustomFilterCo
     public void configure(HttpSecurity builder) throws Exception {
         AuthenticationManager authenticationManager = builder.getSharedObject(AuthenticationManager.class);
 
-        JwtAuthenticationFilter jwtAuthenticationFilter = new JwtAuthenticationFilter(authenticationManager, jwtTokenizer);
+        JwtAuthenticationFilter jwtAuthenticationFilter = new JwtAuthenticationFilter(authenticationManager);
         jwtAuthenticationFilter.setFilterProcessesUrl("/users/login");
-
+        jwtAuthenticationFilter.setAuthenticationSuccessHandler(new MemberAuthenticationSuccessHandler(jwtTokenizer));
+        jwtAuthenticationFilter.setAuthenticationFailureHandler(new MemberAuthenticationFailureHandler());
         builder.addFilter(jwtAuthenticationFilter);
     }
 }
