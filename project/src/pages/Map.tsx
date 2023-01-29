5f8b958f0c9140a29410f618853316e61276c545
부들이 <84694944+namnam-5799@users.noreply.github.com>
2023-01-29 20:42:36 +0900

    Feat attraction detail (#135)
    * fix : git lfs 적용
    
    * feat: Post 카드 컴포넌트
    
    * fix : 페이지네이션 오류 수정
    
    * feat: postcard 컴포넌트 페이지네이션 적용 1차
    
    * design: postcard 컴포넌트 사이즈 조절
    
    * chore : merge
    
    * feat: 카카오맵 위치기반 설정
    
    * design: navbar 여백 수정
    
    * feat: 공유링크 버튼 기능 생성
    
    * feat: 위치기반 검색 버튼 생성
    
    * chore: merge
    
    * feat: 구 별로 데이터 조회
    
    * fix: 카카오맵 api 로직 수정 1차
    
    * feat: map 페이지 장소 렌더링
    
    * feat:map 페이지 컴포넌트 분리
    
    * feat: 마커 전체 렌더링
    
    * feat: map 페이지 마커 이동
    
    * feat: 위치기반 검색시 주변 명소 렌더링
    
    * feat: 마커 중심으로 relayout
    
    * fix: 카카오맵 401 에러 해결
    
    * style : 포스트 없는 경우 css 문구 설정
    
    * chore: 카카오맵 리팩토링 전까지
    
    * fix: 카카오맵 함수 분리 및 useRef
    
    * fix: 카카오맵 마커 렌더링 개선
    
    * style:css 수정
    
    ---------
    
    Co-authored-by: NowVic99 <111376391+NowVic99@users.noreply.github.com>

--- project/src/pages/Map.tsx
+++ project/src/pages/Map.tsx
@@ -118,16 +118,17 @@ const Place = styled.div<{imgUrl:string}>`
     background-size: cover;
 
     >div{
-      padding:85px 0 2px 20px;
+      padding:85px 0 2px 15px;
       font-weight: bold;
       font-size: 18px;
       color:white;
     }
     >p{
-      padding:5px 0 0 20px;
+      padding:5px 0 0 15px;
       font-weight: bold;
-      font-size: 12px;
+      font-size: 11px;
       color:white;
+      margin-bottom: 15px;
     }
 `
 
@@ -144,6 +145,8 @@ const PlaceDetailModalHeader = styled.div`
   height: 370px;
   //background-color: red;
   // 얘는 이미지 
+  //background-color: red;
+  // 얘는 이미지 
   > div:nth-child(1) {
     width: 100%;
     height: 200px;
@@ -153,8 +156,10 @@ const PlaceDetailModalHeader = styled.div`
       background-size: cover;
     }
   }
+  
     >div:nth-child(2){ 
       display: flex;
+      //background-color: yellow;
       //background-color: yellow;
       >p:nth-child(1){
         font-size: 14px;
@@ -242,7 +247,7 @@ const PlaceDetailModalHeader = styled.div`
     font-size: 15px;
     font-weight: bold;
     margin: 20px 0 0 17px;
-    color: #2e3776;
+    color: #0b113f87;
   }
   > span {
     color: #555555;
@@ -260,6 +265,7 @@ const PlaceDetailModalMain = styled.div`
     font-size: 15px;
     font-weight: 700;
     margin: 35px 0 30px 20px;
+    margin: 35px 0 30px 20px;
     color: #393939;
   }
 `;
@@ -272,6 +278,7 @@ const PostImgContainer = styled.div`
 
   > img {
     width: 48%;
+    height: 110px;
     height: 110px;
     margin: 0 2px;
   }
@@ -325,13 +332,8 @@ const Map = () => {
   ];
 
   const url =
-<<<<<<< HEAD
-    "https://pikcha36.o-r.kr:8080/attractions/maps?page=1&size=100&sort=posts";
-  //const url2 = 'http://pikcha36.o-r.kr:8080/attractions/mapdetails/1';
-=======
     "/attractions/maps?page=1&size=99&sort=posts";
   //const url2 = '/attractions/mapdetails/1';
->>>>>>> 6b9275e8cf410e6b0ad06d848344c389f9f3f7e1
   const [filterOrPosition, setFilterOrPosition] = useState<any>(false);
 
   useEffect(() => {
@@ -366,11 +368,7 @@ const Map = () => {
   const handleModalData = (dataUrl: string) => {
     // 모달창 데이터 받아오기
     axios
-<<<<<<< HEAD
-      .get(`https://pikcha36.o-r.kr:8080/attractions/mapdetails/${dataUrl}`)
-=======
       .get(`/attractions/mapdetails/${dataUrl}`)
->>>>>>> 6b9275e8cf410e6b0ad06d848344c389f9f3f7e1
       .then((res) => {
         setModalData(res.data.data);
         console.log("모달데이터", modalData);
