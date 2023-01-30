123bc585c94a318c72c1de5854a0789c2247249a
Sangyoo <tkddbwkd@naver.com>
2023-01-30 20:14:32 +0900

    fix: attraction response isVoted 출력 로직 수정

--- server/src/main/java/com/main36/pikcha/domain/attraction/entity/Attraction.java
+++ server/src/main/java/com/main36/pikcha/domain/attraction/entity/Attraction.java
@@ -22,38 +22,32 @@ public class Attraction extends Auditable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long attractionId;
-
     @Column(name = "attraction_name", nullable = false)
     private String attractionName;
-
-    @Column(name = "likes", nullable = false)
-    @ColumnDefault("0")
-    private Long likes;
-
-    @Column(name = "saves", nullable = false)
-    @ColumnDefault("0")
-    private Long saves;
-
-    @Column(name = "num_of_posts")
-//    @Formula("(SELECT count(1) FROM post p WHERE p.postId = attractionId)")
-    @ColumnDefault("0")
-    private Long numOfPosts;
     @Lob
     @Column( name = "attraction_description", nullable = false)
     private String attractionDescription;
-
     @Column(name = "attraction_address", nullable = false)
     private String attractionAddress;
-
-//    @OneToOne(cascade = {CascadeType.PERSIST, CascadeType.REMOVE})
-//    @JoinColumn(name = "attraction_image_id")
-//    private AttractionImage attractionImage;
+    @Column(name = "likes", nullable = false)
+    @ColumnDefault("0")
+    private Long likes;
+    @Column(name = "saves", nullable = false)
+    @ColumnDefault("0")
+    private Long saves;
+    @Column(name = "num_of_posts")
+    @ColumnDefault("0")
+    private Long numOfPosts;
     @Column(name = "fixed_image")
     private String fixedImage;
-
     @Column(name = "province", nullable = false)
     private String province;
-
+/*    @Column(name = "rank")
+    @ColumnDefault("100")
+    private Long rank;
+    @Column(name = "rank_change", nullable = false)
+    @ColumnDefault("0")
+    private Long rankChange;*/
     @OneToMany(mappedBy = "attraction", cascade = CascadeType.REMOVE)
     private List<Post> posts = new ArrayList<>();
 }
