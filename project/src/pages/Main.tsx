1b321da98735e8f8a834153171c5f57566b0766c
MINSE97 <99317339+MINSE97@users.noreply.github.com>
2023-01-31 14:35:27 +0900

    fix: 정적 API 주소 추가

--- project/src/pages/Main.tsx
+++ project/src/pages/Main.tsx
@@ -93,10 +93,11 @@ function Main() {
   const [isLogin] = useRecoilState(LoginState);
   const memberId = localStorage.getItem("memberId");
 
-  const url1 = "/attractions/filter?page=1&size=4&sort=posts";
-  const url1_LoggedIn = `/attractions/filter/${memberId}?page=1&size=4&sort=posts`;
-  const url2 = `/posts/home?page=1&size=8&sort=views`;
-  const url2_LoggedIn = `/posts/home/${memberId}?page=1&size=8&sort=views`;
+  const url1 =
+    "https://pikcha36.o-r.kr:8080/attractions/filter?page=1&size=4&sort=posts";
+  const url1_LoggedIn = `https://pikcha36.o-r.kr:8080/attractions/filter/${memberId}?page=1&size=4&sort=posts`;
+  const url2 = `https://pikcha36.o-r.kr:8080/posts/home?page=1&size=8&sort=views`;
+  const url2_LoggedIn = `https://pikcha36.o-r.kr:8080/posts/home/${memberId}?page=1&size=8&sort=views`;
 
   useEffect(() => {
     const attraction_url = isLogin ? url1_LoggedIn : url1;
