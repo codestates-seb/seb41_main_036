039680028d3fd5de9c955f5ca53fda612cdd58f1
Lee SeungHyun <tmdgusdlrk2@gmail.com>
2023-02-03 02:29:45 +0900

    fix: 회원가입시 새로고침

---
+++ client/src/pages/Map.tsx
@@ -0,0 +1,572 @@
+import styled from "styled-components";
+import KakaoMap from "../components/KakaoMap";
+import { regionDummy } from "../data/regionData";
+import { useState, useEffect } from "react";
+import { RiArrowDropDownLine } from "react-icons/ri";
+import { FaMapMarkerAlt } from "react-icons/fa";
+import { AiOutlineHeart } from "react-icons/ai";
+import { BsBookmarkPlus, BsFillChatLeftFill } from "react-icons/bs";
+import HiddenHeader from "../components/Header/HiddenHeader";
+import "../index.css";
+import tags from "../data/tagData";
+import axios from "../utils/axiosinstance";
+import { GiTalk } from "react-icons/gi";
+import { useNavigate } from "react-router-dom";
+import { LoginState } from "../recoil/state";
+import { useRecoilState } from "recoil";
+import Modal from "../components/Modal";
+
+const Container = styled.div`
+  display: flex;
+  background-color: white;
+  * {
+    box-sizing: content-box !important;
+  }
+`;
+
+const PlaceList = styled.div`
+  width: 25%;
+  height: 86vh;
+  padding: 10px 0;
+  box-sizing: content-box;
+`;
+
+const DropDown = styled.article`
+  position: absolute;
+  margin-left: 20px;
+  box-sizing: content-box;
+  > button {
+    cursor: pointer;
+    background-color: white;
+    height: 40px;
+    margin-left: 5px;
+    padding-left: 10px;
+    line-height: 40px;
+    display: flex;
+    border: 1px solid var(--black-600);
+    border-radius: var(--br-s);
+    > div:nth-child(1) {
+      width: 150px;
+      height: 40px;
+      text-align: center;
+      font-weight: bold;
+      line-height: 40px;
+      margin-left: 65px;
+    }
+    > div:nth-child(2) {
+      margin-top: 4px;
+      margin-left: 40px;
+    }
+  }
+`;
+
+const SelectList = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 295px;
+  height: 200px;
+  margin-left: 5px;
+  overflow-y: auto;
+  overflow-x: hidden;
+  color: var(--black-750);
+  position: relative;
+  top: 0;
+  border-collapse: collapse;
+  ::-webkit-scrollbar {
+    display: none;
+  }
+  > button {
+    cursor: pointer;
+    font-weight: 500;
+    background-color: #ffffffe5;
+    width: 300px;
+    height: 40px;
+    padding: 10px 0;
+    border-left: 0.5px solid var(--black-500);
+    border-bottom: 0.5px solid var(--black-500);
+    border-top: -0.9px solid var(--black-500);
+    :hover {
+      color: var(--purple-400);
+      background-color: white;
+      font-weight: bold;
+    }
+  }
+`;
+
+const PlaceComponent = styled.div`
+  margin-top: 50px;
+  width: 310px;
+  height: 88vh;
+  margin-left: 26px;
+  background-color: #ffffff;
+  overflow-y: auto;
+  overflow-x: hidden;
+  -ms-overflow-style: none;
+`;
+const Place = styled.div<{ imgUrl: string }>`
+  background-color: skyblue;
+  width: 294px;
+  height: 140px;
+  margin-left: -1px;
+  margin-bottom: 5px;
+  border-radius: var(--br-s);
+  cursor: pointer;
+  border: 1px solid var(--black-600);
+  background: linear-gradient(
+      to right,
+      rgba(20, 20, 20, 0.8) 10%,
+      rgba(20, 20, 20, 0.7) 25%,
+      rgba(20, 20, 20, 0.5) 50%,
+      rgba(20, 20, 20, 0.35) 75%,
+      rgba(20, 20, 20, 0.25) 100%
+    ),
+    url(${(props) => props.imgUrl});
+  background-size: cover;
+  > div {
+    padding: 85px 0 2px 20px;
+    font-weight: bold;
+    font-size: 18px;
+    color: white;
+  }
+  > p {
+    padding: 5px 0 0 15px;
+    font-weight: bold;
+    font-size: 11px;
+    color: white;
+    margin-bottom: 15px;
+    display: flex;
+    align-items: center;
+    svg {
+      margin: 0 5px;
+    }
+  }
+`;
+
+const PlaceDetailModal = styled.div`
+  position: absolute;
+  left: 350px;
+  width: 400px;
+  height: 90vh;
+  border-right: 7px solid #e4dcdc;
+  border-left: 1px solid #e4dcdc;
+`;
+
+const PlaceDetailModalHeader = styled.div`
+  box-sizing: content-box;
+  > div:nth-child(1) {
+    width: 100%;
+    height: 200px;
+    > img {
+      width: 100%;
+      height: 100%;
+      background-size: cover;
+    }
+  }
+  > div:nth-child(2) {
+    display: flex;
+    > p:nth-child(1) {
+      font-size: 14px;
+      margin: 15px 265px 0 20px;
+      line-height: 20px;
+      font-weight: 600;
+      color: var(--black-700);
+    }
+    > div {
+      cursor: pointer;
+      margin-top: 13px;
+      margin-right: 11px;
+    }
+    & p {
+      font-size: 10px;
+      margin-left: 5px;
+      color: #373737;
+    }
+  }
+  > div:nth-child(3) {
+    box-sizing: content-box;
+    display: flex;
+    width: 325px;
+    height: 30px;
+    > h2 {
+      background-color: #fbf8ba;
+      margin-left: 20px;
+      font-weight: 700;
+      margin-bottom: 5px;
+    }
+    > a {
+      font-size: 13px;
+      margin-left: 10px;
+      line-height: 30px;
+      text-decoration: none;
+      color: var(--purple-400);
+      font-weight: 600;
+    }
+  }
+  > div:nth-child(5) {
+    display: flex;
+    color: #919191;
+    font-weight: bold;
+    margin: 10px 0 0 20px;
+    > div {
+      margin-right: 3px;
+    }
+    > p {
+      font-size: 12px;
+      font-weight: 500;
+    }
+  }
+  > p:nth-child(4) {
+    box-sizing: content-box;
+    color: #555555;
+    font-size: 14px;
+    margin: 4px 0 0 20px;
+    font-weight: 600;
+  }
+  > div {
+    margin-right: 5px;
+  }
+  & p {
+    font-size: 10px;
+    margin-left: 5px;
+    color: #373737;
+  }
+  > div:nth-child(5) {
+    box-sizing: content-box;
+    display: flex;
+    color: #919191;
+    font-weight: bold;
+    margin: 8px 0 0 20px;
+    > div {
+      margin-right: 3px;
+    }
+    > p {
+      font-size: 12px;
+      font-weight: 500;
+    }
+  }
+  > div:nth-child(6) {
+    box-sizing: content-box;
+    font-size: 15px;
+    font-weight: bold;
+    margin: 10px 0 15px 20px;
+    color: #0b113f87;
+  }
+  > span {
+    color: #555555;
+    margin-left: 350px;
+    position: absolute;
+    transform: translateY(-10px);
+    cursor: pointer;
+  }
+`;
+
+const PlaceDetailModalMain = styled.div`
+  width: 100%;
+  height: 485px;
+  border-top: 1px solid #e4dcdc;
+  > div:nth-child(1) {
+    box-sizing: content-box;
+    font-size: 15px;
+    font-weight: 700;
+    margin: 17px 0 17px 20px;
+    color: #393939;
+  }
+`;
+
+const PostImgContainer = styled.div`
+  width: 90%;
+  margin: 0 auto;
+  overflow: auto;
+  height: 35vh;
+  > img {
+    width: 48%;
+    height: 110px;
+    height: 110px;
+    margin: 0 2px;
+  }
+`;
+
+const PostNone = styled.div`
+  padding: 10px;
+  width: 300px;
+  height: 100px;
+  font-size: 13px;
+  color: grey;
+  display: flex;
+  > div {
+    margin-right: 5px;
+  }
+`;
+
+  interface RegionType {
+    attractionAddress:string, 
+    attractionId:number,
+    attractionName:string,
+    fixedImage:string
+  }
+
+  interface ModalDataType {
+    attractionAddress:string, 
+    attractionId:number,
+    attractionName:string, 
+    fixedImage:string|undefined, 
+    isSaved:boolean,
+    isVoted:boolean,
+    likes:number,
+    numOfPosts:number,
+    postIdAndUrls: string|number[]
+  }
+
+  interface RegionDummyType {
+    Post:string
+  }
+
+const Map = () => {
+  const [dropdownView, setDropdownView] = useState(false);
+  const [regionFilter, setRegionFilter] = useState("전체");
+  const [detailModal, setDetailModal] = useState(false);
+  const [regionList, setRegionList] = useState<any>(undefined);
+  const [modalData, setModalData] = useState<any>("");
+  const [modalDataId, setModalDataId] = useState<number>(1);
+  const [wholeData, setWholeData] = useState<any>();
+  const navigate = useNavigate();
+  const [filterOrPosition, setFilterOrPosition] = useState<boolean>(false);
+  const [isLogin] = useRecoilState(LoginState);
+  const [isVoted, setIsVoted] = useState<boolean>();
+  const [isLiked, setIsLiked] = useState<boolean>();
+  const [isModalVisible, setIsModalVisible] = useState<boolean>(false);
+  const memberId = localStorage.getItem("memberId");
+  const url = "/attractions/maps?page=1&size=104&sort=posts";
+  const url2 = `/attractions/${modalDataId}`;
+  const url3 = `/attractions/${modalDataId}/${memberId}`;
+  const URL_FOR_SAVES = `/attractions/saves/${modalDataId}`;
+  const URL_FOR_LIKES = `/attractions/likes/${modalDataId}`;
+  const ATTRACTIONS_URL = isLogin ? url3 : url2;
+
+
+  const handleClickLiked = () => {
+    if(isLogin){
+      axios.post(URL_FOR_LIKES).then((res) => {
+        setIsVoted(res.data.data.isVoted);
+        return;
+      }); 
+    }else{
+      setIsModalVisible(true);
+    }
+  }
+
+  const handleClickSaved = () => {
+    if(isLogin){
+      axios.post(URL_FOR_SAVES).then((res) => {
+        setIsLiked(res.data.data.isSaved);
+        return;
+      });
+    }else{
+      setIsModalVisible(true)
+    }
+  }
+
+  useEffect(() => {
+    axios.get(ATTRACTIONS_URL)
+    .then((res)=>{
+      setIsVoted(res.data.data.isVoted)
+      setIsLiked(res.data.data.isSaved)
+    })
+
+    if (regionFilter === "전체") {
+      axios
+        .post(url, {
+          provinces: [],
+        })
+        .then((res) => {
+          setRegionList(res.data.data);
+          setWholeData(res.data.data);
+        });
+    } else {
+      axios
+        .post(url, {
+          provinces: [regionFilter],
+        })
+        .then((res) => {
+          setRegionList(res.data.data);
+        });
+    }
+  }, [regionFilter, setDropdownView, modalData, ATTRACTIONS_URL]);
+
+  const handleModalData = (dataUrl: string | number) => {
+    axios.get(`/attractions/mapdetails/${dataUrl}`).then((res) => {
+      setModalData(res.data.data);
+    });
+  };
+
+  return (
+    <>
+      {isModalVisible && <Modal setIsModalVisible={setIsModalVisible} />}
+      <HiddenHeader></HiddenHeader>
+      <Container>
+        <PlaceList>
+          <DropDown>
+            <button
+              onClick={() => {
+                setDropdownView(!dropdownView);
+              }}
+            >
+              <div>{regionFilter}</div>
+              <div>
+                <RiArrowDropDownLine
+                  size="30"
+                  color="#6255F8"
+                ></RiArrowDropDownLine>
+              </div>
+            </button>
+            {dropdownView ? (
+              <SelectList>
+                {regionDummy.map((el: RegionDummyType, index: number) => {
+                  return (
+                    <button
+                      key={index}
+                      onClick={() => {
+                        setRegionFilter(el.Post);
+                        setDropdownView(false);
+                      }}
+                    >
+                      {el.Post}
+                    </button>
+                  );
+                })}
+              </SelectList>
+            ) : null}
+          </DropDown>
+          <PlaceComponent>
+            {regionList !== undefined &&
+              regionList.map((el: RegionType , index: number) => {
+                return (
+                  <Place
+                    onClick={() => {
+                      setDetailModal(true);
+                      handleModalData(el.attractionId);
+                      setModalDataId(el.attractionId);
+                      setFilterOrPosition(false);
+                    }}
+                    imgUrl={el.fixedImage}
+                    key={el.attractionId}
+                  >
+                    <div>{el.attractionName}</div>
+                    <p>
+                      <FaMapMarkerAlt size="10"></FaMapMarkerAlt>
+                      {el.attractionAddress}
+                    </p>
+                  </Place>
+                );
+              })}
+          </PlaceComponent>
+        </PlaceList>
+        {detailModal ? (
+          <PlaceDetailModal>
+            <PlaceDetailModalHeader>
+              <div>
+                <img src={modalData.fixedImage} alt={'modalImg'}></img>
+              </div>
+              <div>
+                <p>서울 명소</p>
+                <div onClick={handleClickLiked}>
+                  <AiOutlineHeart
+                    color={
+                      isVoted === true
+                        ? "var(--pink-heart)"
+                        : "var(--black-400)"
+                    }
+                    ></AiOutlineHeart>
+                  {/* <p>{ modalData.likes }</p> */}
+                </div>
+                <div onClick={handleClickSaved}>
+                  <BsBookmarkPlus 
+                    color={isLiked ? "green" : "var(--black-400)"}
+                    ></BsBookmarkPlus>
+                  {/* <p>{modalData.saves}</p> */}
+                </div>
+              </div>
+              <div>
+                <h2>{modalData.attractionName}</h2>
+                <a href={"/attractions/detail/" + modalData.attractionId}>
+                  더보기
+                </a>
+              </div>
+              <p>{modalData.attractionAddress}</p>
+              <div>
+                <div>
+                  <BsFillChatLeftFill size="13"></BsFillChatLeftFill>
+                </div>
+                <p>{modalData && modalData.numOfPosts}개의 리뷰</p>
+              </div>
+              <div>
+                {tags[modalData.attractionId % tags.length]}{" "}
+                {tags[(modalData.attractionId - 1) % tags.length]}{" "}
+              </div>
+              <span onClick={() => setDetailModal(false)}>{"<<<"}</span>
+            </PlaceDetailModalHeader>
+            <PlaceDetailModalMain>
+              <div>방문자 포토리뷰</div>
+              <PostImgContainer>
+                {modalData.numOfPosts > 1 ? (
+                  modalData.postIdAndUrls.map((el: any, index: number) => {
+                    return (
+                      <>
+                        <img
+                          src={el.imageUrls}
+                          key={index}
+                          alt={'Img'}
+                          onClick={() => {
+                            navigate(`/posts/detail/${el.postId}`);
+                          }}
+                        ></img>
+                      </>
+                    );
+                  })
+                ) : (
+                  <PostNone>
+                    <div>
+                      <GiTalk size="19"></GiTalk>
+                    </div>
+                    등록된 포토리뷰가 없습니다.
+                  </PostNone>
+                )}
+              </PostImgContainer>
+            </PlaceDetailModalMain>
+          </PlaceDetailModal>
+        ) : null}
+        {detailModal ? (
+          <KakaoMap
+            width="71%"
+            height="94vh"
+            dataList={regionList}
+            position="absolute"
+            left="750px"
+            regionFilter={regionFilter}
+            component="map"
+            dataset={wholeData}
+            modalData={modalData}
+            filterOrPosition={filterOrPosition}
+            setFilterOrPosition={setFilterOrPosition}
+          ></KakaoMap>
+        ) : (
+          <KakaoMap
+            width="100%"
+            height="94vh"
+            dataList={regionList}
+            position="absolute"
+            left="350px"
+            regionFilter={regionFilter}
+            component="map"
+            dataset={wholeData}
+            modalData={modalData}
+            filterOrPosition={filterOrPosition}
+            setFilterOrPosition={setFilterOrPosition}
+          ></KakaoMap>
+        )}
+      </Container>
+    </>
+  );
+};
+
+export default Map;
---
+++ client/src/pages/Place.tsx
@@ -0,0 +1,256 @@
+import { useEffect, useMemo, useRef } from "react";
+import { useLocation } from "react-router-dom";
+import { useState } from "react";
+import styled from "styled-components";
+import LocationFilter from "../components/LocationFilter";
+import { Header } from "../components/Header";
+import axios from "../utils/axiosinstance";
+import PlaceCard from "../components/PlaceCard";
+import Pagination from "../components/Pagination";
+import Footer from "../components/Footer";
+import { useRecoilState } from "recoil";
+import { LoginState } from "../recoil/state";
+import EmptyResult from "../components/EmptyResult";
+
+const PlaceWrapper = styled.div`
+  display: flex;
+  max-width: 1280px;
+  width: 83.5%;
+  margin: 0 auto;
+  padding-top: 70px;
+`;
+
+const LocationWrapper = styled.nav`
+  min-width: 190px;
+  max-height: 850px;
+  border-radius: var(--br-m);
+  border: 1px solid var(--black-200);
+  overflow: hidden;
+  margin-top: 10px;
+  background-color: transparent;
+  margin-bottom: 20px;
+  overflow-y: auto;
+  height: 100%;
+  background-color: var(--black-200);
+`;
+
+const PlaceContainer = styled.div`
+  min-height: 790px;
+  margin: 20px 0 20px 30px;
+  width: 100%;
+`;
+
+const PlaceFilterContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-left: 5px;
+  height: 50px;
+  padding-bottom: 10px;
+  > span {
+    font-size: var(--font-sm);
+    color: var(--black-800);
+    font-weight: var(--fw-medium);
+  }
+`;
+
+export const FilterButton = styled.button`
+  margin: 0 10px;
+  padding-bottom: 3px;
+  border: none;
+  background-color: transparent;
+  color: var(--black-900);
+  font-weight: var(--fw-bold);
+  cursor: pointer;
+  &.active {
+    color: var(--purple-400);
+    border-bottom: 1px solid var(--purple-300);
+  }
+`;
+
+const PlaceBox = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 25px 2%;
+`;
+const ITEM_LIMIT = 9;
+
+export interface PlaceType {
+  attractionId: number;
+  attractionName: string;
+  fixedImage: string;
+  likes: number;
+  numOfPosts: number;
+  saves: number;
+  isSaved: boolean;
+  isVoted: boolean;
+}
+
+export interface PageInfoType {
+  page: number;
+  size: number;
+  totalElements: number;
+  totalPages: number;
+}
+
+const sortList: { kor: string; eng: string }[] = [
+  {
+    kor: "최신순",
+    eng: "newest",
+  },
+  {
+    kor: "리뷰순",
+    eng: "posts",
+  },
+  {
+    kor: "인기순",
+    eng: "likes",
+  },
+];
+
+export interface ArrayPlaceType extends Array<PlaceType> {}
+
+const Place = () => {
+  const [placesData, setPlacesData] = useState<ArrayPlaceType>();
+  const [checkedList, setCheckedlist] = useState<string[]>([]);
+  const [onFilter, setOnFliter] = useState(0);
+  const [curPage, setCurPage] = useState(1);
+  const [sort, setSort] = useState("newest");
+  const { search } = useLocation();
+  const totalInfoRef = useRef<PageInfoType | null>(null);
+
+  const [isLogin] = useRecoilState(LoginState);
+  const memberId = localStorage.getItem("memberId");
+
+  const searchValue = useMemo(
+    () => new URLSearchParams(search).get("keyword"),
+    [search]
+  );
+
+  const url1 = `/attractions/search?keyword=${searchValue}&page=${curPage}&size=${ITEM_LIMIT}&sort=${sort}`;
+  const url1_loggedIn = `/attractions/search/${memberId}?keyword=${searchValue}&page=${curPage}&size=${ITEM_LIMIT}&sort=${sort}`;
+  const url2 = `/attractions/filter?page=${curPage}&size=${ITEM_LIMIT}&sort=${sort}`;
+  const url2_loggedIn = `/attractions/filter/${memberId}?page=${curPage}&size=${ITEM_LIMIT}&sort=${sort}`;
+
+  useEffect(() => {
+    setCurPage(1);
+  }, [checkedList]);
+
+  useEffect(() => {
+    const search_url = isLogin ? url1_loggedIn : url1;
+    const url = isLogin ? url2_loggedIn : url2;
+    if (searchValue) {
+      axios
+        .post(search_url, { provinces: checkedList })
+        .then((res) => {
+          setPlacesData(res.data.data);
+          totalInfoRef.current = res.data.pageInfo;
+        })
+        .catch((err) => console.error(err));
+    } else {
+      axios
+        .post(url, {
+          provinces: checkedList,
+        })
+        .then((res) => {
+          setPlacesData(res.data.data);
+          totalInfoRef.current = res.data.pageInfo;
+        })
+        .catch((err) => console.error(err));
+      return;
+    }
+  }, [searchValue, curPage, checkedList, sort]);
+
+  const handleSortClick = (idx: number, sort: string) => {
+    setOnFliter(idx);
+    setSort(sort);
+  };
+
+  return (
+    <>
+      <div style={{ display: "fixed" }}>
+        <Header headerColor="var(--black-200)">
+          <Header.HeaderTop />
+          <Header.HeaderBody
+            defaultValue={searchValue ? searchValue : undefined}
+            selectedMenu={0}
+          />
+        </Header>
+      </div>
+      <PlaceWrapper>
+        <LocationWrapper>
+          {placesData && (
+            <LocationFilter
+              setCurPage={setCurPage}
+              checkedList={checkedList}
+              setCheckedList={setCheckedlist}
+            />
+          )}
+        </LocationWrapper>
+        <PlaceContainer>
+          <PlaceFilterContainer>
+            {searchValue ? (
+              <span>
+                <strong
+                  style={{ marginRight: "5px" }}
+                >{`'${searchValue}' 검색결과`}</strong>{" "}
+                {`총 ${totalInfoRef.current?.totalElements}개의 명소`}{" "}
+              </span>
+            ) : (
+              <span>총 {totalInfoRef.current?.totalElements}개의 명소</span>
+            )}
+
+            <div>
+              {sortList.map((sort, idx) => (
+                <FilterButton
+                  className={onFilter === idx ? "active" : ""}
+                  key={idx}
+                  onClick={() => {
+                    handleSortClick(idx, sort.eng);
+                  }}
+                >
+                  {sort.kor}
+                </FilterButton>
+              ))}
+            </div>
+          </PlaceFilterContainer>
+          <PlaceBox>
+            {!totalInfoRef.current?.totalElements && searchValue && (
+              <EmptyResult
+                message="다른 검색어를 입력해보세요"
+                subtitle={false}
+              />
+            )}
+            {placesData?.length ? (
+              placesData.map((placeInfo) => (
+                <PlaceCard
+                  key={placeInfo.attractionId}
+                  placeInfo={placeInfo}
+                  width="32%"
+                />
+              ))
+            ) : (
+              <>
+                {!searchValue && (
+                  <EmptyResult
+                    message="등록된 명소가 없습니다"
+                    subtitle={false}
+                  />
+                )}
+              </>
+            )}
+          </PlaceBox>
+          {placesData && (
+            <Pagination
+              props={totalInfoRef.current as PageInfoType}
+              setCurPage={setCurPage}
+            />
+          )}
+        </PlaceContainer>
+      </PlaceWrapper>
+      <Footer />
+    </>
+  );
+};
+
+export default Place;
