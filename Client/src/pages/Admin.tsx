06e80c6e974d86df0d8a331a83adbddde006066a
Lee SeungHyun <tmdgusdlrk2@gmail.com>
2023-02-22 02:33:01 +0900

    feat: 헤더, 바디, 푸터 적용

--- Client/src/pages/Admin.tsx
+++ Client/src/pages/Admin.tsx
@@ -3,19 +3,27 @@ import React, { useState, useEffect } from "react";
 import axios from "../utils/axiosinstance";
 import { useNavigate } from "react-router-dom";
 import FixedOnScrollUpHeader from "../components/Header/FixedOnScrollUpHeader";
+import Footer from "../components/Footer";
 
-
+const AdminWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`;
 
 function Admin() {
+  return (
+    <>
+      <FixedOnScrollUpHeader />
+      <AdminWrapper>
+        <div>임시 관리자 페이지</div>
+        <div>임시 관리자 페이지</div>
+        <div>임시 관리자 페이지</div>
+        <div>임시 관리자 페이지</div>
+      </AdminWrapper>
+      <Footer />
+    </>
+  );
+}
 
-    return (
-      <>
-    <FixedOnScrollUpHeader />  
-    <div>임시 관리자 페이지</div>
-
-      </>
-    );
-  }
-  
-  export default Admin;
-  
+export default Admin;
