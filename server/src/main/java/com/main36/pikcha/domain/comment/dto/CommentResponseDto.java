574d7e3cd23744f58d94bceb589fc39096ba1f90
Sangyoo <tkddbwkd@naver.com>
2023-02-26 20:40:42 +0900

    fix: 댓글 responseDtoBuilder에 parentId 추가

--- server/src/main/java/com/main36/pikcha/domain/comment/dto/CommentResponseDto.java
+++ server/src/main/java/com/main36/pikcha/domain/comment/dto/CommentResponseDto.java
@@ -17,9 +17,10 @@ public class CommentResponseDto {
     private LocalDateTime modifiedAt;
 
     @Builder
-    public CommentResponseDto(Long commentId, Long memberId, String username, String memberPicture,
+    public CommentResponseDto(Long commentId, Long parentId, Long memberId, String username, String memberPicture,
                               String commentContent, LocalDateTime createdAt, LocalDateTime modifiedAt) {
         this.commentId = commentId;
+        this.parentId = parentId;
         this.memberId = memberId;
         this.username = username;
         this.memberPicture = memberPicture;
