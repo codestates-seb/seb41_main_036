3e65c16e2f933a73a93459966753abcc78260c4a
milLee <tkddb7347@gmail.com>
2023-02-27 02:00:14 +0900

    Update deploy.sh

--- server/scripts/deploy.sh
+++ server/scripts/deploy.sh
@@ -7,10 +7,6 @@ echo "> 현재 시간: $(date)" >> /home/ubuntu/action/deploy.log
 
 echo "> build 파일명: $JAR_NAME" >> /home/ubuntu/action/deploy.log
 
-echo "> build 파일 복사" >> /home/ubuntu/action/deploy.log
-DEPLOY_PATH=/home/ubuntu/action/
-cp $BUILD_JAR $DEPLOY_PATH
-
 echo "> 현재 실행중인 애플리케이션 pid 확인" >> /home/ubuntu/action/deploy.log
 CURRENT_PID=$(pgrep -f $JAR_NAME)
 
@@ -23,7 +19,7 @@ else
   sleep 5
 fi
 
-
-DEPLOY_JAR=$DEPLOY_PATH$JAR_NAME
+# DEPLOY_PATH=/home/ubuntu/action/server/build/libs/
+# DEPLOY_JAR=$DEPLOY_PATH$JAR_NAME
 echo "> DEPLOY_JAR 배포"    >> /home/ubuntu/action/deploy.log
-sudo nohup java -jar $DEPLOY_JAR >> /home/ubuntu/deploy.log 2>/home/ubuntu/deploy_err.log &
+sudo nohup java -jar $BUILD_JAR >> /home/ubuntu/deploy.log 2>/home/ubuntu/deploy_err.log &
