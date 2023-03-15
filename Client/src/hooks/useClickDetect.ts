db182044d305813c487a024ed396feb72c150abe
useusername1 <reknvqvjoq123@gmail.com>
2023-03-15 17:24:39 +0900

    refactor:매개변수 추가

--- Client/src/hooks/useClickDetect.ts
+++ Client/src/hooks/useClickDetect.ts
@@ -1,9 +1,9 @@
 import { useRef, useState, useEffect } from "react";
 
 //ref요소가 click된 요소의 자식이 아니면 isvisible을 false로 만든다
-function useClickDetect() {
+function useClickDetect(defaultVisibleState: boolean = false) {
   const ref = useRef<HTMLElement | null>(null);
-  const [isVisible, setIsVisible] = useState(false);
+  const [isVisible, setIsVisible] = useState(defaultVisibleState);
 
   const handleClick = (e: MouseEvent) => {
     if (isVisible && !ref.current!.contains(e.target as Node)) {
