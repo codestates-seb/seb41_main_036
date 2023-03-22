5d530cbe8da948d961a315bfcbd1a39b4801cb23
useusername1 <reknvqvjoq123@gmail.com>
2023-03-23 00:31:04 +0900

    chore:푸터에 추가할 깃헙 주소 데이터 생성

---
+++ Client/src/data/contributorData.ts
@@ -0,0 +1,9 @@
+const contributors = [
+  { name: "김진주", link: "https://github.com/useusername1" },
+  { name: "박혜수", link: "https://github.com/namnam-5799" },
+  { name: "이동우", link: "https://github.com/eastmeet" },
+  { name: "이상유", link: "https://github.com/Sangyoo" },
+  { name: "이승현", link: "https://github.com/NowVic99" },
+  { name: "정세민", link: "https://github.com/MINSE97" },
+];
+export default contributors;
