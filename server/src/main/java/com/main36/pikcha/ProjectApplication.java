1e534ecddfcc44efd0e0557e23f687f914b7bfed
eastmeet <ys932184@gmail.com>
2023-04-06 19:43:09 +0900

    [Chore] @PropertySource(classpath:env.yml) 추가

--- server/src/main/java/com/main36/pikcha/ProjectApplication.java
+++ server/src/main/java/com/main36/pikcha/ProjectApplication.java
@@ -3,11 +3,13 @@ package com.main36.pikcha;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
 @EnableBatchProcessing
+@PropertySource("classpath:env.yml")
 public class ProjectApplication {
 
     public static void main(String[] args) {
