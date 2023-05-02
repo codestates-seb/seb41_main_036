a10a834202e81381c674ed0147d24be047256d38
useusername1 <reknvqvjoq123@gmail.com>
2023-05-03 00:16:32 +0900

    refactor:채팅 버튼 적용으로 chatpanel로 코드 이동

--- Client/src/components/Chat/index.tsx
+++ Client/src/components/Chat/index.tsx
@@ -1,59 +1,19 @@
-import {
-  useState,
-  useEffect,
-  MouseEvent,
-  ChangeEvent,
-  KeyboardEvent,
-} from "react";
-import useWebsocket from "../../hooks/useWebsocket";
-const URL = "https://pikcha36.o-r.kr:8080/stomp-websocket-sockjs"; //wss
-const TOPIC = "/topic/messages";
+import ChatButton from "./ChatButton";
+import ChatPanel from "./ChatPanel";
+import { useRecoilValue } from "recoil";
+import { ChatStatus } from "../../recoil/ChatState";
+
 const Chat = () => {
-  const { clientRef, chatData, setChatData, sendMessage } = useWebsocket(
-    URL,
-    TOPIC
-  );
-  const [text, setText] = useState("");
-  const [isComposing, setIsComposing] = useState(false);
-  useEffect(() => {
-    console.log(chatData, "서버응답 body");
-  }, [chatData]);
+  const chatStatus = useRecoilValue(ChatStatus);
 
-  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setText((e.target as HTMLInputElement).value);
-  };
-
-  const clickHandler = (
-    e: MouseEvent<HTMLButtonElement, globalThis.MouseEvent>
-  ) => {
-    e.preventDefault();
-    sendMessage(text);
-    setText("");
-  };
-
-  const handleInputKeyDown = (e: KeyboardEvent<HTMLInputElement>) => {
-    if (isComposing) return;
-    if (e.key === "Enter") {
-      sendMessage(text);
-      setText("");
-    }
-  };
   return (
     <>
-      <div>채팅 테스트</div>
-      {chatData.map((el, i) => (
-        <div key={i}> {el.content} </div>
-      ))}
-      <input
-        type="text"
-        value={text}
-        onChange={handleInputChange}
-        onKeyDown={handleInputKeyDown}
-        onCompositionStart={() => setIsComposing(true)}
-        onCompositionEnd={() => setIsComposing(false)}
-      ></input>
-      <button onClick={(e) => clickHandler(e)}>전송</button>
+      {(chatStatus === "EXITED" || chatStatus === "MINIMIZED") && (
+        <ChatButton />
+      )}
+      {chatStatus !== "EXITED" && <ChatPanel chatStatus={chatStatus} />}
     </>
   );
 };
 export default Chat;
+/*minimized에서는 새메시지 도착하면 버튼 변경 */
