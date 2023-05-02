ba50027248f0dab739c6090435f4e12444f416c2
useusername1 <reknvqvjoq123@gmail.com>
2023-05-02 23:54:27 +0900

    feat:채팅 입력창 컴포넌트 추가

---
+++ Client/src/components/Chat/Send/SendStyled.tsx
@@ -0,0 +1,164 @@
+import styled, { css } from "styled-components";
+import { sendbarStyleType } from "../Chat";
+export type SendBarButtonType = "emoji" | "send";
+const EmojipickerWrapper = styled.div<{ rowNum: number }>`
+  position: absolute;
+  right: 0;
+  transform: ${(props) =>
+    `translateY(calc(-50% - 15px - var(--sb-padding) - var(--sb-lineheight) * ${props.rowNum}/2))`};
+  emoji-picker {
+    --emoji-font-family: "Apple Color Emoji";
+  }
+`;
+const ReplyInfoDiv = styled.div`
+  display: flex;
+  align-items: center;
+  margin: 0 5px 13px 7px;
+  div.reply-info {
+    margin-right: 10px;
+  }
+  div.reply-message {
+    color: var(--black-800);
+    font-size: var(--font-xs);
+    word-break: break-all;
+  }
+  div.reply-user {
+    margin-top: 3px;
+    font-size: var(--font-xs);
+    font-weight: var(--fw-medium);
+    color: var(--black-600);
+  }
+  span {
+    color: var(--pink-heart);
+    margin-left: auto;
+    svg {
+      width: 17px;
+      height: 17px;
+    }
+    :hover {
+      cursor: pointer;
+    }
+  }
+`;
+const SendbarButton = styled.div<{
+  buttontype: SendBarButtonType;
+  rowNum: number;
+  showEmoji?: boolean;
+  disabled?: boolean;
+}>`
+  pointer-events: ${(props) => (props.disabled ? "none" : "auto")};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-grow: 0.5;
+  background-color: var(--chatbox-background);
+  border: none;
+  width: 10%;
+  transition: background-color ease 0.1s;
+  height: ${(props) =>
+    `calc(${props.rowNum} * var(--sb-lineheight) + var(--sb-padding) * 2)`};
+  div.emoji-icon {
+    transition: all ease 0.2s;
+    filter: grayscale(100%);
+    font-size: var(--font-md);
+  }
+  svg.send-icon {
+    transition: all ease 0.2s;
+    transform: scale(1.1);
+    color: ${(props) =>
+      props.disabled ? "var(--black-400)" : "var(--black-800)"};
+  }
+  ${(props) =>
+    props.buttontype === "emoji" &&
+    css`
+      border-radius: 0;
+    `}
+  ${(props) =>
+    props.buttontype === "send" &&
+    css`
+      border-radius: 0 15px 15px 0;
+    `}
+    ${(props) =>
+    props.showEmoji &&
+    css`
+      div.emoji-icon {
+        font-size: var(--font-xl);
+        filter: none;
+      }
+    `}
+      :hover {
+    cursor: ${(props) => (props.disabled ? "default" : "pointer")};
+    background-color: ${(props) =>
+      props.disabled ? "var(--chatbox-background)" : "var(--black-275)"};
+    div.emoji-icon {
+      font-size: var(--font-xl);
+      filter: none;
+    }
+  }
+`;
+
+const SendBarFrameDiv = styled.div<{
+  styleProps: sendbarStyleType;
+  showSearchBox?: boolean;
+}>`
+  --sb-padding: ${(props) => `${props.styleProps.padding}px`};
+  --sb-lineheight: ${(props) => `${props.styleProps.lineheight}px`};
+  width: 100%;
+  padding: 15px;
+  background-color: white;
+  position: sticky;
+  bottom: 0;
+  border-radius: ${(props) =>
+    props.showSearchBox ? "0 0 15px 0" : "0 0 15px 15px"};
+`;
+
+const SendBarDiv = styled.div<{
+  disabled: boolean;
+  rowNum: number;
+}>`
+  display: flex;
+  width: 100%;
+  textarea {
+    background-color: var(--chatbox-background);
+    font-family: "Pretendard Variable";
+    display: block;
+    flex-grow: 9.2;
+    border-radius: 15px 0 0 15px;
+    border: none;
+    padding: var(--sb-padding) 0 var(--sb-padding) 15px;
+    resize: none;
+    overflow: auto;
+    line-height: var(--sb-lineheight);
+    scrollbar-gutter: stable;
+    scroll-padding-bottom: var(--sb-padding);
+    font-size: var(--font-xs);
+    cursor: auto;
+    :focus {
+      outline: none;
+    }
+    ::-webkit-scrollbar {
+      width: 8px;
+    }
+    ::webkit-scrollbar-gutter {
+      scrollbar-gutter: auto;
+    }
+    ::-webkit-scrollbar-track {
+      background-color: transparent;
+    }
+    ::-webkit-scrollbar-thumb {
+      background-color: var(--chatbox-background);
+      border-radius: 5px;
+    }
+    :hover::-webkit-scrollbar-thumb {
+      background-color: var(--black-400);
+    }
+  }
+`;
+
+export {
+  EmojipickerWrapper,
+  ReplyInfoDiv,
+  SendbarButton,
+  SendBarDiv,
+  SendBarFrameDiv,
+};
---
+++ Client/src/components/Chat/Send/Sendbar.tsx
@@ -0,0 +1,220 @@
+import { useState, ChangeEvent, KeyboardEvent, FormEvent, useRef } from "react";
+import { flushSync } from "react-dom";
+import { useRecoilState, useRecoilValue } from "recoil";
+import { ShowSearchBox, isReplyMessageState } from "../../../recoil/ChatState";
+import EmojiPicker, {
+  EmojiClickData,
+  EmojiStyle,
+  SuggestionMode,
+} from "emoji-picker-react";
+import useClickDetect from "../../../hooks/useClickDetect";
+
+import { generateRandomEmoji } from "../../../utils/utils";
+import { DividerLine } from "../ChatStyled";
+import {
+  EmojipickerWrapper,
+  ReplyInfoDiv,
+  SendBarDiv,
+  SendBarFrameDiv,
+  SendbarButton,
+} from "./SendStyled";
+import { RiSendPlaneFill as SendIcon } from "react-icons/ri";
+import { IoClose as CloseIcon } from "react-icons/io5";
+import { TbArrowForward as ReplyIcon } from "react-icons/tb";
+import { chatDatatype, sendbarStyleType } from "../Chat";
+
+export const sendbarStyle: sendbarStyleType = {
+  padding: 10,
+  lineheight: 20,
+};
+
+interface SendbarProps {
+  sendMessage: (text: string) => void;
+  replyMessage: (text: string, targetInfo: chatDatatype) => void;
+  scrollIntoBottom: () => void;
+}
+const Sendbar = ({
+  sendMessage,
+  replyMessage,
+  scrollIntoBottom,
+}: SendbarProps) => {
+  const {
+    ref: emojiRef,
+    isVisible: showEmoji,
+    setIsVisible: setShowEmoji,
+  } = useClickDetect();
+  const [isReplyMessage, setIsReplyMessage] =
+    useRecoilState(isReplyMessageState);
+  const [text, setText] = useState("");
+  const [rowNum, setRowNum] = useState(1);
+  const [emoji, setEmoji] = useState("🙂");
+  const textareaRef = useRef<HTMLTextAreaElement | null>(null);
+  const showSearchBox = useRecoilValue(ShowSearchBox);
+  const handleInputChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
+    setText((e.target as HTMLTextAreaElement).value);
+  };
+
+  const clickHandler = (
+    e: React.MouseEvent<HTMLDivElement, globalThis.MouseEvent>
+  ) => {
+    e.preventDefault();
+
+    flushSync(() => {
+      if (text.trim().length === 0) return;
+      if (isReplyMessage) {
+        replyMessage(text, isReplyMessage);
+        setIsReplyMessage(null);
+      } else {
+        sendMessage(text);
+      }
+      setText("");
+      setRowNum(1);
+    });
+    scrollIntoBottom();
+  };
+
+  const handleInputKeyDown = (e: KeyboardEvent<HTMLTextAreaElement>) => {
+    if (e.nativeEvent.isComposing) return;
+
+    if (e.key === "Enter") {
+      e.preventDefault();
+      flushSync(() => {
+        if (text.trim().length === 0) return;
+        if (isReplyMessage) {
+          replyMessage(text, isReplyMessage);
+          setIsReplyMessage(null);
+        } else {
+          sendMessage(text);
+        }
+        setText("");
+        setRowNum(1);
+      });
+
+      scrollIntoBottom();
+    }
+  };
+
+  function handleOnInput(e: FormEvent<HTMLTextAreaElement>) {
+    const target = e.target as HTMLTextAreaElement;
+
+    if (target.scrollHeight > target.clientHeight && rowNum < 5) {
+      setRowNum((p) => p + 1);
+      return; //다음줄로 넘어가면 row늘리기
+    }
+    if (target.scrollHeight === target.clientHeight && rowNum !== 1) {
+      target.style.height = "0";
+      const newRowNum = Math.ceil(
+        (target.scrollHeight - sendbarStyle.padding * 2) /
+          sendbarStyle.lineheight
+      );
+      target.style.cssText = "";
+      if (newRowNum < rowNum) setRowNum(newRowNum);
+    }
+  }
+
+  const handleEmojiClick = (emojiData: EmojiClickData, event: MouseEvent) => {
+    flushSync(() => {
+      setText((p) => p + emojiData.emoji);
+    });
+
+    if (
+      (textareaRef.current as HTMLTextAreaElement).scrollHeight >
+        (textareaRef.current as HTMLTextAreaElement).clientHeight &&
+      rowNum < 5
+    ) {
+      setRowNum((p) => p + 1);
+      return; //다음줄로 넘어가면 row늘리기
+    }
+    (textareaRef.current as HTMLTextAreaElement).scrollTop = (
+      textareaRef.current as HTMLTextAreaElement
+    ).scrollHeight;
+  };
+
+  const handleMouseOver = () => {
+    if (showEmoji) return;
+    setEmoji(generateRandomEmoji());
+  };
+
+  const handleEmojiPickerClick = (e: React.MouseEvent) => {
+    setShowEmoji((p) => !p);
+  };
+
+  const handleEmojipickerWrapperClick = (e: React.MouseEvent) => {
+    e.stopPropagation();
+  };
+  const handleCloseClick = () => {
+    setIsReplyMessage(null);
+  };
+  return (
+    <>
+      <SendBarFrameDiv styleProps={sendbarStyle} showSearchBox={showSearchBox}>
+        {isReplyMessage !== null && (
+          <>
+            <ReplyInfoDiv>
+              <div className="reply-info">
+                <div className="reply-message">{isReplyMessage.content}</div>
+                <div className="reply-user">
+                  <ReplyIcon />
+                  {Number(localStorage.getItem("memberId")) ===
+                  isReplyMessage.memberId
+                    ? "나"
+                    : `${isReplyMessage.username}님`}
+                  에게 답장
+                </div>
+              </div>
+              <span onClick={handleCloseClick} className="close-icon">
+                <CloseIcon />
+              </span>
+            </ReplyInfoDiv>
+            <DividerLine />
+          </>
+        )}
+        <SendBarDiv disabled={text.trim().length === 0} rowNum={rowNum}>
+          <textarea
+            key={1}
+            ref={textareaRef}
+            rows={rowNum}
+            value={text}
+            onInput={handleOnInput}
+            onChange={handleInputChange}
+            onKeyDown={handleInputKeyDown}
+          ></textarea>
+          <SendbarButton
+            ref={emojiRef as React.RefObject<HTMLDivElement>}
+            buttontype="emoji"
+            rowNum={rowNum}
+            onMouseEnter={handleMouseOver}
+            onClick={handleEmojiPickerClick}
+            showEmoji={showEmoji}
+          >
+            <div className="emoji-icon">{emoji}</div>
+            <EmojipickerWrapper
+              onClick={handleEmojipickerWrapperClick}
+              rowNum={rowNum}
+            >
+              {showEmoji && (
+                <EmojiPicker
+                  height={420}
+                  onEmojiClick={handleEmojiClick}
+                  autoFocusSearch={false}
+                  suggestedEmojisMode={SuggestionMode.RECENT}
+                  lazyLoadEmojis={true}
+                  emojiStyle={EmojiStyle.NATIVE}
+                />
+              )}
+            </EmojipickerWrapper>
+          </SendbarButton>
+          <SendbarButton
+            buttontype="send"
+            rowNum={rowNum}
+            onClick={(e) => clickHandler(e)}
+            disabled={text.trim().length === 0}
+          >
+            <SendIcon className="send-icon" />
+          </SendbarButton>
+        </SendBarDiv>
+      </SendBarFrameDiv>
+    </>
+  );
+};
+export default Sendbar;
